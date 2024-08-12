0b990678f0a54b61cd45f6b97d87a0ba09fccc09
Gabriel Albano <albanogabriel33@gmail.com>
2024-08-12 14:43:54 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
+import { SearchIcon } from "lucide-react"
+import { Header } from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>BUtton</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gabriel</h2>
+        <p>Segunda-feira, 05 de agosto</p>
+
+        <div className="mt-6 flex flex-row gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="banner agende"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }

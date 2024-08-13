244a08a0b286c3494e79a214a1f40bf34a2d99e0
Gabriel Albano <albanogabriel33@gmail.com>
2024-08-13 13:51:49 -0300

    feat: add constant to map searchOptions and add svg icons to these searchOptions

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOptionsProps {
+  imgUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOptionsProps[] = [
+  {
+    imgUrl: "/scissors.svg",
+    title: "Cabelo",
+  },
+  {
+    imgUrl: "/mustache.svg",
+    title: "Barba",
+  },
+  {
+    imgUrl: "/razor.svg",
+    title: "Acabamento",
+  },
+  {
+    imgUrl: "/eyebrows.svg",
+    title: "Sobrancelhas",
+  },
+  {
+    imgUrl: "/massage.svg",
+    title: "Massagem",
+  },
+  {
+    imgUrl: "/hydration.svg",
+    title: "Hidratação",
+  },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,15 @@ import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import { BarbershopItem } from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
 
   return (
     <div>
@@ -28,6 +34,23 @@ const Home = async () => {
           <Button>
             <SearchIcon />
           </Button>
+        </div>
+
+        {/* QUICK SEARCH */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => {
+            return (
+              <Button key={option.title} className="gap-2" variant="secondary">
+                <Image
+                  src={option.imgUrl}
+                  alt={option.title}
+                  width={16}
+                  height={16}
+                />
+                {option.title}
+              </Button>
+            )
+          })}
         </div>
 
         {/* BANNER */}
@@ -67,16 +90,36 @@ const Home = async () => {
           </CardContent>
         </Card>
 
+        {/* recomendados */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
-
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        {/* Popular */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Popular
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card className="rounded-none">
+          <CardContent className="px-5 py-6">
+            <p className="text-xs text-gray-400">
+              2023 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }

8a94efa6ec7a7af3a2c9abae79f492eaedf3fddc
Gabriel Albano <albanogabriel33@gmail.com>
2024-08-16 16:01:27 -0300

    chore: starting nextAuth setup

--- app/_components/sidebar-content.tsx
+++ app/_components/sidebar-content.tsx
@@ -1,10 +1,19 @@
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 export function SidebarContent() {
   return (
@@ -13,8 +22,33 @@ export function SidebarContent() {
         <SheetTitle>Menu</SheetTitle>
 
         {/* AVATAR */}
-        <div className="flex items-center gap-3 border-b border-solid py-5 text-left">
-          <Avatar>
+        <div className="flex items-center justify-between gap-3 border-b border-solid py-5 text-left">
+          <h2 className="text-left font-bold">Olá faça o seu login!</h2>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>
+                <LogInIcon size={18} />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="w-[90%]">
+                <DialogTitle>Faça login na plataforma</DialogTitle>
+                <DialogDescription>
+                  Contecte-se usando sua conta do Google.
+                </DialogDescription>
+              </DialogHeader>
+              <Button variant={"outline"} className="gap-1">
+                <Image
+                  src={"/google.svg"}
+                  alt="google"
+                  width={18}
+                  height={18}
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
+          {/* <Avatar>
             <AvatarImage
               className="object-cover"
               src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -23,7 +57,7 @@ export function SidebarContent() {
           <div>
             <p className="font-bold">Marina Andrade</p>
             <p className="text-xs">marina.andrade@gmail.com</p>
-          </div>
+          </div> */}
         </div>
 
         <div className="flex flex-col gap-4 border-b border-solid py-4">
